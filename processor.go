b920b723014d9809b9fa996bbf39b92f4c30e288
Srinivas Sakhamuri <srinisakh@gmail.com>
2022-05-11 21:27:14 -0400

    fixed the bug

--- processor.go
+++ processor.go
@@ -2,7 +2,6 @@ package telegraf
 
 type Processor interface {
 	// SampleConfig returns the default configuration of the Input
-	SampleConfig() string
 
 	// Description returns a one-sentence description on the Input
 	Description() string
